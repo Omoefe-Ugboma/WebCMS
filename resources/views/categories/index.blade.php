66d5450f52987d40406dc465fe4fd5901be18b04
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2020-01-24 11:37:53 +0100

    deleting category and displaying a Modal with javascript and bootstrap

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -23,12 +23,59 @@
                 <a href="{{route('categories.edit',$category->id)}}" class="btn btn-info btn-sm">
                   Edit
                 </a>
+
+                 <button class="btn btn-danger btn-sm" onclick="handleDelete({{$category->id}})">Delete</button>
+
               </tg>
            </tr>
          @endforeach
         </tbody>
       </table>
+
+      <form action="" method="POST" id="deleteCategoryForm"> 
+          
+          @csrf
+          
+          @method('DELETE')
+        <!-- Modal -->
+        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                
+                <h4 class="modal-title" id="deleteModalLabel">Delete Category</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              </div>
+              <div class="modal-body">
+                <p class="text-center text-bold">
+                  Are you sure want to delete this category?
+                </p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">No, Go back</button>
+                <button type="submit" class="btn btn-danger">Yes, Delete</button>
+              </div>
+            </div>
+          </div>
+      </div>
+
+
+      </form>
+
       </div>
    </div>
 
+@endsection
+
+@section('scripts')
+  <script>
+    function handleDelete(id){
+      
+
+      var form = document.getElementById('deleteCategoryForm')
+      form.action = '/categories/' +id
+       
+      $('#deleteModal').modal('show')
+    }
+  </script>
 @endsection
